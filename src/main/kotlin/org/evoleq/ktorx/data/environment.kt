64279b5fecb4a4b59b147ebb62d0a018293cc152
Florian Schmidt <florian.schmidt@platis.de>
2020-10-10 09:37:27 +0200

    Added environment.kt

---
+++ src/main/kotlin/org/evoleq/ktorx/data/environment.kt
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2020 Dr. Florian Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.evoleq.ktorx.data
+
+/**
+ * Abstract the environment of an application
+ */
+open class Environment {
+    companion object {
+        private lateinit var innerType: EnvironmentType
+    
+        /**
+         * Type of the [Environment]: dev, prod, test, ...
+         */
+        var Type: EnvironmentType
+            get() = innerType
+            set(value) {
+                if (!::innerType.isInitialized) {
+                    innerType = value
+                }
+            }
+    }
+}
+
+/**
+ * Type of the [Environment]: dev, prod, test, ...
+ */
+interface EnvironmentType { val name: String }
+
+/**
+ * UnknownEnvironmentTypeException
+ */
+class UnknownEnvironmentTypeException(private val type: EnvironmentType? = null) : Exception(
+    """|Unknown environment type${if(type != null){": ${type.name}"}else{""}}
+    """.trimMargin()
+)
